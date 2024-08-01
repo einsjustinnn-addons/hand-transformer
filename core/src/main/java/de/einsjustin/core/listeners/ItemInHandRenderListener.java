26d068fee99dcd81f1a8b513cfae7ba98bc17e48
EinsJustinnn <z0ck3r98hd@gmail.com>
2024-08-01 20:36:22 +0200

    change item size which only changes in first person

--- core/src/main/java/de/einsjustin/core/listeners/ItemInHandRenderListener.java
+++ core/src/main/java/de/einsjustin/core/listeners/ItemInHandRenderListener.java
@@ -2,6 +2,7 @@ package de.einsjustin.core.listeners;
 
 import de.einsjustin.core.HandTransformerAddon;
 import de.einsjustin.core.event.ItemInHandRenderEvent;
+import net.labymod.api.client.render.model.ModelTransformType;
 import net.labymod.api.event.Phase;
 import net.labymod.api.event.Subscribe;
 
@@ -16,6 +17,8 @@ public class ItemInHandRenderListener {
   @Subscribe
   public void onItemInHandRender(ItemInHandRenderEvent event) {
     if (event.phase() != Phase.PRE) return;
+    ModelTransformType type = event.type();
+    if (!(type == ModelTransformType.FIRST_PERSON_RIGHT_HAND || type == ModelTransformType.FIRST_PERSON_LEFT_HAND)) return;
     float size = this.addon.configuration().itemSize().get();
     var stack = event.stack();
     stack.scale(size);
