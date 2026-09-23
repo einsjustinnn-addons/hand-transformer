b6c3c17a954f5fe6b3c829c5401fdb0fa7901f30
Justin <me@jxtn.de>
2026-09-23 18:33:17 +0200

    update LabyGradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
 group = "org.example"
-version = System.getenv().getOrDefault("VERSION", "1.1.0")
+version = providers.environmentVariable("VERSION").getOrElse("1.0.0")
 
 labyMod {
     defaultPackageName = "de.einsjustin.handtransformer" // change this to your main package name (used by all modules)
